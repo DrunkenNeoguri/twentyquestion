f04cf0047a46765883db2e8fb4a980984b817c20
developNeoguri <developneoguri@gmail.com>
2023-10-22 11:44:18 +0900

    Init. Resetting initial web page state

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { css } from "../../styled-system/css";
-
-export default function Home() {
-  return <div>Hello 🐼!</div>;
-}
--- src/app/test.ts
+++
@@ -1,5 +0,0 @@
-export const dummyTest = {
-  nickname: "너구리",
-  questionChallengeCount: 108,
-  questionAnswerCount: 31,
-};
